cd0dbefc441c6c2ebea646d0f9504d63d8feeb0e
rj2024 <rajatjana2020@gmail.com>
2023-08-24 10:56:10 +0530

    first commit from windows machine

---
+++ day1/first.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define ld long double
+
+const ll N = 1e5 + 5;
+void go()
+{
+    cout<<"hello world"<<endl;
+}
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+   #ifndef ONLINE_JUDGE
+       freopen("inputf.in", "r", stdin);
+       freopen("outputf.in", "w", stdout);
+   #endif
+    int t = 1;
+    cin >> t;
+    while (t--){
+        go();
+    }
+    return 0;
+}
